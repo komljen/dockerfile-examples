afa3e382a973e1626ec0aee5b1226ccb5946ef27
Alen Komljen <alen.komljen@live.com>
2013-12-01 20:09:38 +0100

    Small fixes on multiple images

--- rails-sample-app/start.sh
+++ rails-sample-app/start.sh
@@ -7,10 +7,11 @@
 echo "Pull latest changes:"
 cd $APP_ROOT
 git pull origin master
+cp $APP_ROOT/config/database.yml.example $APP_ROOT/config/database.yml
 bundle install
 rake db:migrate
 #-------------------------------------------------------------------------------
 echo "Starting rails:"
 source /usr/local/rvm/scripts/rvm
-rails server
+rails s
 #===============================================================================
--- wordpress/start.sh
+++ wordpress/start.sh
@@ -13,21 +13,18 @@ done
 echo "Creating database:"
 ./create_db.sh
 #-------------------------------------------------------------------------------
-echo "Copy wordpress content to /var/www volume:"
-cp -r wordpress/* $APP_ROOT
-#-------------------------------------------------------------------------------
 sed -e "s/database_name_here/$WP_DB/
 s/username_here/$WP_USER/
 s/password_here/$WP_PASS/
 s/localhost/$MYSQL_PORT_3306_TCP_ADDR/
-/'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-/'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-/'LOGGED_IN_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-/'NONCE_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-/'AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-/'SECURE_AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-/'LOGGED_IN_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-/'NONCE_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/"              \
+/'AUTH_KEY'/s/put your unique phrase here/$(pwgen -c -n -1 65)/
+/'SECURE_AUTH_KEY'/s/put your unique phrase here/$(pwgen -c -n -1 65)/
+/'LOGGED_IN_KEY'/s/put your unique phrase here/$(pwgen -c -n -1 65)/
+/'NONCE_KEY'/s/put your unique phrase here/$(pwgen -c -n -1 65)/
+/'AUTH_SALT'/s/put your unique phrase here/$(pwgen -c -n -1 65)/
+/'SECURE_AUTH_SALT'/s/put your unique phrase here/$(pwgen -c -n -1 65)/
+/'LOGGED_IN_SALT'/s/put your unique phrase here/$(pwgen -c -n -1 65)/
+/'NONCE_SALT'/s/put your unique phrase here/$(pwgen -c -n -1 65)/"             \
 $APP_ROOT/wp-config-sample.php > $APP_ROOT/wp-config.php
 
 chown -R www-data: $APP_ROOT
